50769914aff050d2debcb69e703af0c64939f052
Nilcire <alun5236@gmail.com>
2026-01-17 02:39:10 +0800

    build(deps): add coil-compose dependency for AsyncImage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.logging.interceptor)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
