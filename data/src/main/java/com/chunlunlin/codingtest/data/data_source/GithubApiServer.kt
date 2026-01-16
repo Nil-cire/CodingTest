36fbdec97db567a10ffd6bde2085a0b6ad120d4c
Nilcire <alun5236@gmail.com>
2026-01-16 12:17:57 +0800

    feat(data): implement GithubApiService and UserDto

---
+++ data/src/main/java/com/chunlunlin/codingtest/data/data_source/GithubApiServer.kt
@@ -0,0 +1,16 @@
+package com.chunlunlin.codingtest.data.data_source
+
+import com.chunlunlin.codingtest.data.dto.GithubUserDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubApiServer {
+
+    @GET("users")
+    suspend fun getUsers(): List<GithubUserDto>
+
+    @GET("users/{login}")
+    suspend fun getUserDetail(
+        @Path("login") login: String
+    ): GithubUserDto
+}
