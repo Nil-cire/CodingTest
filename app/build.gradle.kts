b388153987d6879f5ca9d5ef078cb43020c3dfbb
Nilcire <alun5236@gmail.com>
2026-01-16 22:58:54 +0800

    build(deps): add Compose Navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.converter.gson)
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
